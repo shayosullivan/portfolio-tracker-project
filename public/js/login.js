fec4b65b2ca350084d86bde2ec3fe465a007a5e0
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-06-23 12:02:22 -0400

    add document navigation function if correct login

--- public/js/login.js
+++ public/js/login.js
@@ -11,6 +11,11 @@ const loginFormHandler = async (event) => {
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
+    }
 }
 }
 
