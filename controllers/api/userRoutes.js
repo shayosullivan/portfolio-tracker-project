b96b1bd11b12df28f43c871cbd4f00396c3a54f0
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-06-23 14:57:32 -0400

    add in function for if user info is correct

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,12 @@ const { User } = require('../../models');
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
-
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
 
   } catch (err) {
     res.status(400).json(err);
