7da6a42bd28ae901b882be8329868593ace5f1c8
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-06-23 14:56:15 -0400

    add function to find user based on email

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
