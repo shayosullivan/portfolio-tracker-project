bde9bb913f21860395d4be212aff5f82d4b068fe
lyf703331869 <yliu52@nyit.edu>
2022-06-25 10:29:49 -0400

    Update the files

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const yahooFinance = require('yahoo-finance');
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  res.render('homepage', {
+    loggedIn: req.session.loggedIn
+  })
+});
+
+router.get('/price', withAuth, (req, res) => {
+  const symbol = req.query.symbol;
+  if (!symbol) {
+    return res.status(404).send('Not found');
+  }
+  yahooFinance.quote(
+    {
+      symbol: symbol,
+      modules: ['financialData'],
+    },
+    function (err, quotes) {
+      if (quotes && quotes.financialData && quotes.financialData.currentPrice) {
+        res.send({
+          symbol: symbol,
+          price: quotes.financialData.currentPrice,
+        });
+      } else {
+        return res.status(404).send('Not found');
+      }
+    }
+  );
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  res.render('dashboard');
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const sequelize = require("../config/connection");
+const bcrypt = require('bcrypt');
+const { Model, DataTypes, Sequelize } = require("sequelize");
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+};
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [6],
+        },
+    },
+},
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+
+    });
+module.exports = User;
