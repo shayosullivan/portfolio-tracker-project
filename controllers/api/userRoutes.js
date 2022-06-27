832f69dba507addda00a73ccc5c1cbcfc707062e
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-06-27 12:21:05 -0400

    add functionality to the login and register

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// CREATE new user
-
-// ROUTE: /api/users
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
